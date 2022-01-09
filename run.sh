7046983e6dbe8fac0d70be3e315d74f63b8c4a28
201702083 <201702083@o.cnu.ac.kr>
2022-01-10 00:52:27 +0900

    Make dev_db container, dev & down command

--- run.sh
+++ run.sh
@@ -4,37 +4,55 @@ cmd=$1
 where=$2
 opt=$3
 case "$cmd" in
-	-up)
-	case "$where" in
+	-up) # web server
+	case "$2" in
 		express)
 			cd express-server
-			npm run $opt
+			npm run $3 # check express-server/package.json
 			;;
-				
 		gin)
 			cd gin-server 
-			go run main.go $opt
+			if [ "$3" == "dev" ] 
+			then
+				gin run main.go $3
+			fi
 			;;
-
 		*)
-		echo "'$where' is unknown server"
+		echo "'$2' is unknown server"
 		;;
 	esac
 	;;
-	-reset)
-	if [ "$where" == "" ] && [ "$opt" == "" ] ; then
-		docker compose down -v
-		rm -rf db/data/
-	else
-			echo "'$where' and '$opt'"
+	-reset) # DB reset
+		if [ "$3" == "" ] ; then
+			if [ "$2" == "dev" ] ; then	# rm dev db volumn
+				docker compose down dev_db redis -v
+				rm -rf dev/data/
+			elif [ "$2" == "" ]; then # rm real db volumn
+				docker compose down db redis -v
+				rm -rf db/data/
+			fi
+		else
+				echo "'$2' and '$3'"
 
-	fi
-	echo "gdgd"
+		fi
 	;;
-	-init)
-		docker compose up
+	-init) # DB, redis compose up
+		if [ "$2" == "dev" ] ; then # dev, mock dummy db 
+			docker compose up -d dev_db redis 
+		elif [ "$2" == "" ] ; then  # real db
+			docker compose up -d db redis 
+		else
+			echo "'$2' is unknown option"
+		fi
+	;;
+	-down) # DB, redis compose down
+		if [ "$2" == "" ] ; then
+			docker compose down
+		else
+			echo "-down : No option"
+		fi
 	;;
 	*)
-	echo "'$cmd' is unknown command"
+	echo "'$cmd' is unknown command + '$2' and '$3'"
 	;;
 esac
