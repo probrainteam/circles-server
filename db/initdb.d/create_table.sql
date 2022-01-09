08ef550edf9b268c71ce0be45f29a6edd302648a
201702083 <201702083@o.cnu.ac.kr>
2022-01-09 23:43:41 +0900

    pw would be hashed

--- db/initdb.d/create_table.sql
+++ db/initdb.d/create_table.sql
@@ -1,19 +1,19 @@
 CREATE TABLE manager (
     `email`   VARCHAR(30)  NOT NULL,
-    `pw`      VARCHAR(25)  NOT NULL,
+    `pw`      BLOB         NOT NULL,
     `circle`  INT          NOT NULL,
     `pubkey`  BLOB         NOT NULL,
     PRIMARY KEY (circle)
 );
 CREATE TABLE circle (
-    `student_id`BLOB         NOT NULL,
-    `circle`    INT          NOT NULL,
-    `major`     VARCHAR(12)  NOT NULL,
-    `name`      BLOB         NOT NULL,
-    `year`      INT          NOT NULL,
-    `email`     BLOB         NOT NULL,
-    `phone`     BLOB         NOT NULL,
-    `pay`       BOOL         NOT NULL,
-    `status`    INT          NOT NULL,
+    `student_id` BLOB         NOT NULL,
+    `circle`     INT          NOT NULL,
+    `major`      VARCHAR(12)  NOT NULL,
+    `name`       BLOB         NOT NULL,
+    `year`       INT          NOT NULL,
+    `email`      BLOB         NOT NULL,
+    `phone`      BLOB         NOT NULL,
+    `pay`        BOOL         NOT NULL,
+    `status`     INT          NOT NULL,
     PRIMARY KEY (student_id)
 );
