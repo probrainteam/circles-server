10d6a8dbde217351b4989860819a76c5fb47405e
201702083 <201702083@o.cnu.ac.kr>
2022-01-10 00:51:19 +0900

    dummy data load

--- dev/initdb.d/load_data.sql
+++
@@ -1,1 +0,0 @@
--- LOAD DATA INFILE '/Users/macbook/circles-server/db/initdb.d/manager.csv' INTO TABLE manage.manager FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
