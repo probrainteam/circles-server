9289647abca665061d2fd9c562bfd69fec67d42e
201702083 <201702083@o.cnu.ac.kr>
2022-01-19 20:47:33 +0900

    conflict

---
+++ gin-server/modules/api/component.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	. "circlesServer/modules/reader"
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func getCircle(num uint64) string {
+	return []string{"probrain", "grow", "argos", "adm2n", "ana", "motion", "spg", "pai"}[num]
+}
+
+func getAddr() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
+}
+
+func getCircleNum(c *gin.Context) (uint64, error) {
+	access := strings.Split(c.Request.Header.Get("Authorization"), " ")[1]
+	claims := jwt.MapClaims{}
+	_, _ = jwt.ParseWithClaims(access, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(GetConfig(`token.ACCESS_SECRET`)), nil
+	})
+	for key, val := range claims {
+		fmt.Printf("Key: %v, value: %v\n", key, val)
+	}
+	num, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+	if err != nil {
+		return 10, err
+	}
+	return num, nil
+}
