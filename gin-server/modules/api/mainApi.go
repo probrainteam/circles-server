880a68281d28c028e9cc2aa75bb3b7a902ef9955
201702083 <201702083@o.cnu.ac.kr>
2022-01-19 20:36:28 +0900

    2 query call -> 1 query, reduce response time

--- gin-server/modules/api/mainApi.go
+++ gin-server/modules/api/mainApi.go
@@ -4,94 +4,77 @@ import (
 	ErrChecker "circlesServer/modules/errors"
 	. "circlesServer/modules/storage"
 	"errors"
-	"io"
-	"log"
-	"net"
-	"os"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-func getAddr() string {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
+func GetMemberList(c *gin.Context) ([]Member, error) {
+	num, err := getCircleNum(c)
 	if err != nil {
-		log.Fatal(err)
+		return []Member{}, err
 	}
-	defer conn.Close()
-
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	return localAddr.IP.String()
-}
-func GetMemberList(c *gin.Context) ([]Project, error) {
+	circle := getCircle(num)
 	db := DB()
-	var length int
-	_ = db.QueryRow(`your query or GORM`).Scan(&length)
-	if length == 0 {
-		return []Project{}, errors.New("Nothing to show")
-	}
-	rows, err := db.Query(`your query or GORM`)
+	rows, err := db.Query(`select * from ` + circle)
 	if err := ErrChecker.Check(err); err != nil {
-		return []Project{}, err
+		return []Member{}, err
 	}
 	defer rows.Close()
-	projects := make([]Project, 0)
-	var pos Project
+	Members := make([]Member, 0)
+	var mem Member
 	for rows.Next() {
-		err := rows.Scan(&pos.PID, &pos.UID, &pos.TITLE,
-			&pos.TOTAL, &pos.DESCRIPTION, &pos.DUE, &pos.TERM, &pos.PATH)
+		err := rows.Scan(&mem.SID, &mem.MAJOR, &mem.NAME, &mem.YEAR,
+			&mem.EMAIL, &mem.PHONE, &mem.PAID, &mem.STATUS)
 		if err := ErrChecker.Check(err); err != nil {
-			return []Project{}, err
+			return []Member{}, err
 		}
-		projects = append(projects, pos)
+		Members = append(Members, mem)
 	}
-	return projects, nil
+	if len(Members) == 0 {
+		return []Member{}, errors.New("nothing to show")
+	}
+	return Members, nil
 }
 func AddMember(c *gin.Context) (int, error) {
-	var reqBody AddMemberForm
+	var reqBody Member
 
 	err := c.ShouldBind(&reqBody)
 	if err != nil {
 		return 1, err
 	}
 
-	file, _, err := c.Request.FormFile("content")
+	// file, _, err := c.Request.FormFile("content")
 
-	var pid int
+	// var pid int
 	db := DB()
-	db.QueryRow(`your query or GORM`)
-	basePath := "http://" + getAddr()
-	localPath := "/Users/macbook/Sites" // custom
-	dirPath := "/project_img/"          // custom
-	path := basePath + dirPath + strconv.Itoa(pid+1) + `.png`
+	// basePath := "http://" + getAddr()
+	// localPath := "/Users/macbook/Sites" // custom
+	// dirPath := "/Member_img/"           // custom
+	// path := basePath + dirPath + strconv.Itoa(pid+1) + `.png`
 
-	if err != nil {
-		return -1, err
-	}
-	_, err = db.Exec(`your query or GORM`)
+	_, err = db.Exec(`insert into `)
 	if err := ErrChecker.Check(err); err != nil {
 		return -1, err
 	}
 
-	path = localPath + dirPath + strconv.Itoa(pid+1) + `.png`
-	dst, err := os.Create(path)
-	if err != nil {
-		return -1, err
-	}
-	defer dst.Close()
+	// path = localPath + dirPath + strconv.Itoa(pid+1) + `.png`
+	// dst, err := os.Create(path)
+	// if err != nil {
+	// 	return -1, err
+	// }
+	// defer dst.Close()
 
 	if err != nil {
 		return -1, err
 	}
-	if _, err := io.Copy(dst, file); err != nil {
-		return -1, err
-	}
+	// if _, err := io.Copy(dst, file); err != nil {
+	// 	return -1, err
+	// }
 	_, err = db.Exec(`your query or GORM`)
 	if err := ErrChecker.Check(err); err != nil {
 		return -1, err
 	}
-	return pid, nil
+	return 0, nil
 }
 func Deny(c *gin.Context) error {
 	var reqBody ReplyJoinForm
@@ -131,22 +114,22 @@ func Permit(c *gin.Context) error {
 	}
 	return nil
 }
-func Join(c *gin.Context) ([]member, error) {
-	var reqBody JoinForm
+func Join(c *gin.Context) ([]Member, error) {
+	var reqBody Member
 	err := c.ShouldBindJSON(&reqBody)
 	if err := ErrChecker.Check(err); err != nil {
-		return []member{}, err
+		return []Member{}, err
 	}
 	db := DB()
 	_, err = db.Exec(`your query or GORM`)
 	if err != nil {
-		return []member{}, err
+		return []Member{}, err
 	}
-	list := make([]member, 0)
+	list := make([]Member, 0)
 	return list, nil
 }
-func GetNumProject(c *gin.Context) (int, error) {
-	var reqBody JoinForm
+func GetNumMember(c *gin.Context) (int, error) {
+	var reqBody Member
 	err := c.ShouldBindJSON(&reqBody)
 	if err := ErrChecker.Check(err); err != nil {
 		return -1, err
