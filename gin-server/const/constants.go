cb4d0b6c8f02d14c94d03ad445b6aff696c000a2
201702083 <201702083@o.cnu.ac.kr>
2022-01-22 18:41:17 +0900

    constants

---
+++ gin-server/const/constants.go
@@ -0,0 +1,9 @@
+package constant
+
+const (
+	ACCESSUUID  string = "access_uuid"
+	REFRESHUUID string = "refresh_uuid"
+	USERID      string = "user_id"
+	AUTH        string = "authorized"
+	EXPIRE      string = "exp"
+)
