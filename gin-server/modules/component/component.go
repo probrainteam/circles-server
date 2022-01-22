511cca64e37a86b396cc5fcc3a79db6dcc265a4e
201702083 <201702083@o.cnu.ac.kr>
2022-01-22 18:41:53 +0900

    Get uuid func

--- gin-server/modules/component/component.go
+++ gin-server/modules/component/component.go
@@ -1,12 +1,15 @@
 package component
 
 import (
+	. "circlesServer/const"
 	. "circlesServer/modules/reader"
 	"errors"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 )
@@ -25,11 +28,9 @@ func GetAddr() string {
 	return localAddr.IP.String()
 }
 
-func GetCircleNumAcc(access string) (uint64, error) {
-	claims := jwt.MapClaims{}
-	_, _ = jwt.ParseWithClaims(access, claims, func(token *jwt.Token) (interface{}, error) {
-		return []byte(GetConfig(`token.ACCESS_SECRET`)), nil
-	})
+func GetCircleNum(r *http.Request, access bool) (uint64, error) {
+	encToken := strings.Split(r.Header.Get("Authorization"), " ")[1]
+	claims := ReadToken(encToken, access)
 	num, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
 	if err != nil {
 		return 10, err
@@ -39,17 +40,26 @@ func GetCircleNumAcc(access string) (uint64, error) {
 	}
 	return num, nil
 }
-func GetCircleNumRef(refresh string) (uint64, error) {
+
+func GetUuid(r *http.Request, access bool) (string, error) {
+	encToken := strings.Split(r.Header.Get("Authorization"), " ")[1]
+	claims := ReadToken(encToken, access)
+	var uuid string
+	if access {
+		uuid = claims[ACCESSUUID].(string)
+	} else {
+		uuid = claims[REFRESHUUID].(string)
+	}
+	return uuid, nil
+}
+func ReadToken(token string, access bool) jwt.MapClaims {
 	claims := jwt.MapClaims{}
-	_, _ = jwt.ParseWithClaims(refresh, claims, func(token *jwt.Token) (interface{}, error) {
-		return []byte(GetConfig(`token.REFRESH_SECRET`)), nil
+	_, _ = jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		if access {
+			return []byte(GetConfig(`token.ACCESS_SECRET`)), nil
+		} else {
+			return []byte(GetConfig(`token.REFRESH_SECRET`)), nil
+		}
 	})
-	num, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
-	if err != nil {
-		return 10, err
-	}
-	if num > 7 {
-		return 10, errors.New("cicle is not matched yet")
-	}
-	return num, nil
+	return jwt.MapClaims{}
 }
